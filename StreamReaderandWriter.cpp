26c842c1d5ebd070e027b304a32030146d44ce32
nanayyayaa <jiejueejuju@gmail.com>
2026-06-01 19:07:36 +0700

    commit 3

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -13,4 +13,18 @@ int main()
     ofstream outfile;
     //menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis file
+    while (true)
+    {
+
+        cout << "- ";
+        //mendapat setiap karakter dalam satu baris
+        getline(cin, baris);
+        
+
+    }
+
 }
